35e0b7a540fddbb45e66f2e3915548fcb6c2b4e4
Catborisovv <69162936+DEV-Catborisovv@users.noreply.github.com>
2023-05-08 16:35:25 +0500

    Добавлена обработка и валидация данных при отправке POST запроса по методу host/newuser

--- API/Handlers/NewUser/newuser.go
+++ API/Handlers/NewUser/newuser.go
@@ -6,24 +6,46 @@ package newuser
 import (
 	errrorhandler "PetAPI/pkg/ErrrorHandler"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
-func NewUserHandler(w http.ResponseWriter, r *http.Request) {
-	// Запуск горутины (запуск функции в отдельном потоке, чтобы не останавливать работу {API})
-	if r.Method == http.MethodPost {
-		body, _ := ioutil.ReadAll(r.Body)
-		fmt.Println(string(body))
-		if r.URL.Query().Get("name") != "" || r.URL.Query().Get("password") != "" {
+// Функция валидации данных пользователя
+func dataValidation(name string) bool {
+	pattern := `^[a-zA-Z0-9]+$`
+	re := regexp.MustCompile(pattern)
 
+	if re.MatchString(name) {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Обработчик события создания нового пользователя
+func NewUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			log.Printf("Возникла ошибка при получении тела запроса:\n%v\n", err)
+		}
+
+		var nickname string = strings.TrimSpace(r.FormValue("name"))
+		var pass string = strings.TrimSpace(r.FormValue("password"))
+
+		// Проверка переданных данных на валидность
+		if dataValidation(nickname) == true && dataValidation(pass) == true && len(nickname) != 0 && len(pass) != 0 {
+			w.WriteHeader(200)
+			fmt.Println(r)
 		} else {
 			errJson := errrorhandler.GetErrorJson(400, "Data in URL is not valid")
 			w.WriteHeader(400)
 			w.Write([]byte(errJson))
 		}
 	} else {
-		errJson := errrorhandler.GetErrorJson(405, "The GET method is not supported for this API method")
+		errJson := errrorhandler.GetErrorJson(405, fmt.Sprintf("The %s method is not supported for this API method", r.Method))
 		w.WriteHeader(405)
 		w.Write([]byte(errJson))
 	}
